ed28bb0efb5b81f61e19c911e5119f16e4f53924
Didier Ferreira <didierpg@gmail.com>
2024-11-02 14:30:26 -0300

    refactor: move `TaskAdder` component to inside `TaskContainer` component`

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import "./App.css";
 import { Header } from "./components/Header";
-import { TaskAdder } from "./components/TaskAdder";
 import { TaskContainer } from "./components/TaskContainer";
 
 function App() {
   return (
     <>
       <Header />
-      <TaskAdder />
+
       <TaskContainer />
     </>
   );
--- src/components/TaskContainer/index.tsx
+++ src/components/TaskContainer/index.tsx
@@ -1,10 +1,12 @@
 import styles from "./styles.module.css";
+import { TaskAdder } from "./TaskAdder";
 import { TaskList } from "./TaskList";
 import { TaskSummary } from "./TaskSummary";
 
 export function TaskContainer() {
   return (
     <div className={styles.taskcontainer}>
+      <TaskAdder />
       <TaskSummary />
       <TaskList />
     </div>
