cfef7d703d4234c9a5e01dbe1292ae64ac2ea36f
Didier Ferreira <didierpg@gmail.com>
2024-11-04 18:16:44 -0300

    fix: adjust image import paths to use `src/` alias

--- src/components/App/Header/index.tsx
+++ src/components/App/Header/index.tsx
@@ -1,9 +1,9 @@
 import styles from "./styles.module.css";
-
+import rocketImage from "src/assets/rocket.svg";
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src="src/assets/rocket.svg" />
+      <img src={rocketImage} />
       <strong>
         <span>to</span>
         <span>do</span>
--- src/components/App/TaskContainer/TaskList/EmptyList/index.tsx
+++ src/components/App/TaskContainer/TaskList/EmptyList/index.tsx
@@ -1,9 +1,9 @@
 import styles from "./styles.module.css";
-
+import clipboardImage from "src/assets/clipboard.svg";
 export function EmptyList() {
   return (
     <div className={styles.emptylist}>
-      <img src="src/assets/clipboard.svg" />
+      <img src={clipboardImage} />
       <p>
         <strong>Você ainda não tem tarefas cadastradas</strong>
         <span>Crie tarefas e organize seus itens a fazer</span>
