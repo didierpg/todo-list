de50fc8e6bac2ba351a7439f3d121c9951726743
Didier Ferreira <didierpg@gmail.com>
2024-11-04 17:19:11 -0300

    refactor: move `App` component to App folder inside components folder
    - move all components into App folder
    - move all content of `index.css` into App/styles.css to be the main CSS file.
    - delete `index.css`

---
+++ src/components/App/index.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+import { Header } from "./Header";
+import { TaskContainer } from "./TaskContainer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TaskContainer />
+    </>
+  );
+}
+
+export default App;
